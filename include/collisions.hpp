381c2a22faf8bf2b6514d4e06441cf1eb6b82e5f
João Pedro Porto Pires de Oliveira <joaopppobr@gmail.com>
2024-08-19 09:26:04 -0300

    Coloca colisões para colisions.cpp

--- include/collisions.hpp
+++ include/collisions.hpp
@@ -13,4 +13,8 @@
 // inicialmente não intersecciona o plano. Um tempo negativo indica que os dois objetos nunca
 // irão colidir.
 float CalculateAABBToPlaneCollisionTime(float plane_height, float bbox_min_height, float vel);
-
+void CheckSwordCollision();
+void UpdateBoundingBox(BoundingBox &box, glm::vec3 position, glm::vec3 scale); //Faz update do bounding box do inimigo pra teste de colisão
+bool CheckCollision(const BoundingBox &box1, const BoundingBox &box2);
+bool CheckPlayerEnemyCollision();
+bool CheckPlayerTreeCollision() ;
